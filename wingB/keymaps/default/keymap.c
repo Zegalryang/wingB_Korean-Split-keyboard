a31eb8343bd3ec7de8ef0f14d9e9e22b39aa9f7e
Zegalryang <zegalryang@gmail.com>
2022-06-28 20:17:35 +0900

    [Layout] 키보드 레이아웃 반영
    OSX에서 사용하는 특수키 조합을 위한 Function key 반영

--- wingB/keymaps/default/keymap.c
+++ wingB/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB ,    KC_Q  , KC_W    , KC_E      , KC_R   , KC_T  ,              KC_Y      , KC_U   , KC_I     , KC_O   , KC_P    ,KC_EQL,
      KC_CAPS,    KC_A  , KC_S    , KC_D      , KC_F   , KC_G  ,              KC_H      , KC_J   , KC_K     , KC_L   , KC_SCLN ,KC_QUOT,
      KC_LSFT ,   KC_Z  , KC_X    , KC_C      , KC_V   , KC_B  ,              KC_B      , KC_N   , KC_M     , KC_COMM, KC_DOT  ,KC_SLSH,
-     KC_LCTL ,  KC_ESC , KC_LOPT , KC_LCMD   , KC_SPC , MO(_LOWER),          MO(_LOWER), KC_SPC , KC_RCMD  , KC_ROPT, KC_RCTL ,KC_RSFT,
+     KC_LCTL ,  KC_ESC , KC_LOPT , KC_LCMD   , KC_SPC , MO(_LOWER),          MO(_RAISE), KC_SPC , KC_RCMD  , KC_ROPT, KC_RCTL ,KC_RSFT,
                                                KC_BSPC, KC_ENT,              KC_ENT    , KC_BSPC
   ),
   [_LOWER] = LAYOUT(
@@ -23,9 +23,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC,_______, KC_UP ,_______,_______,_______,                         _______, KC_7  , KC_8    , KC_9     ,_______ ,KC_BSLS,
      _______,KC_LEFT,KC_DOWN,KC_RGHT,_______,KC_LBRC,                        KC_RBRC, KC_4  , KC_5    , KC_6     ,_______ ,_______,
      _______,KC_HOME,KC_PGUP,KC_PGDN,_______,KC_LPRN,                        KC_RPRN, KC_1  , KC_2    , KC_3     ,_______ ,_______,
-     _______,KC_MUTE,KC_VOLU,KC_VOLD,_______,_______,                        _______, KC_0  , KC_LEFT ,KC_UP     , KC_DOWN,KC_RGHT,
+     KC_LCTL,KC_ESC ,KC_LOPT,KC_LCMD,_______,_______,                        _______, KC_0  , KC_LEFT ,KC_UP     , KC_DOWN,KC_RGHT,
                                      KC_DEL ,_______,                        _______, KC_DEL
-
+  ),
+  [_RAISE] = LAYOUT(
+     _______,_______,_______,_______,_______,_______,                       _______,_______,_______,_______,_______,_______,
+     _______,_______,_______,_______,_______,_______,                       _______,_______,_______,_______,_______,_______,
+     _______,_______,_______,_______,_______,_______,                       _______,_______,_______,_______,_______,_______,
+     _______,_______,_______,_______,_______,_______,                       _______,_______,_______,_______,_______,_______,
+     _______,KC_MUTE,KC_VOLD,KC_VOLU,_______,_______,                       _______,_______,_______,_______,_______,_______,
+                                     KC_DEL ,_______,                       _______, KC_DEL
   )
 };
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
