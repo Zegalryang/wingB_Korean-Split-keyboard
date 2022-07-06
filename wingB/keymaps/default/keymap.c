88258de5d24c8fc30b828b7154a72e3829628feb
Zegalryang <zegalryang@gmail.com>
2022-07-06 22:52:30 +0900

    [Layout] 펑션키 위치 변경
    숫자와 펑션키의 위치가 헷갈려서 바꿈

--- wingB/keymaps/default/keymap.c
+++ wingB/keymaps/default/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                KC_BSPC, KC_ENT,              KC_ENT    , KC_BSPC
   ),
   [_LOWER] = LAYOUT(
-     KC_F1 , KC_F2 ,  KC_F3  , KC_F4 , KC_F5 , KC_F6 ,                       KC_F7,   KC_F8 , KC_F9   , KC_F10   ,KC_F11  ,KC_F12 ,
+     KC_F2 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                         KC_F6,   KC_F7 , KC_F8   , KC_F9   ,KC_F10  ,KC_F11 ,
      KC_ESC,_______,_______,_______,_______,_______,                         _______, KC_7  , KC_8    , KC_9     ,_______ ,KC_BSLS,
      _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_4  , KC_5    , KC_6     ,_______ ,_______,
      _______,_______,_______,_______,_______,KC_LPRN,                        KC_RPRN, KC_1  , KC_2    , KC_3     ,_______ ,_______,
@@ -27,8 +27,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                      KC_DEL ,_______,                        _______, KC_DEL
   ),
   [_RAISE] = LAYOUT(
-     KC_PSCR,KC_BRMD,KC_PAUS,_______,_______,_______,                       _______,_______,_______,_______,_______,_______,
-     _______,KC_PGUP,KC_UP  ,KC_PGDN,KC_INS ,KC_HOME,                       _______,_______,_______,_______,_______,_______,
+     KC_ESC ,KC_PSCR,KC_BRMD,KC_PAUS,_______,_______,                       _______,_______,_______,_______,_______,_______,
+     _______,KC_PGDN,KC_UP  ,KC_PGUP,KC_INS ,KC_HOME,                       _______,_______,_______,_______,_______,_______,
      _______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,                       _______,_______,_______,_______,_______,_______,
      _______,_______,_______,_______,_______,_______,                       _______,_______,_______,_______,_______,_______,
      _______,KC_MUTE,KC_VOLD,KC_VOLU,_______,_______,                       _______,_______,_______,_______,_______,_______,
